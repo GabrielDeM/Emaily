8388d41915c102307357d94d75323f08f6dcf335
unknown <gabrielmouracanto@hotmail.com>
2019-01-21 15:01:48 +0100

    added github keys

--- config/prod.js
+++ config/prod.js
@@ -3,8 +3,8 @@ module.exports = {
 	googleClientID: process.env.GOOGLE_CLIENT_ID,
 	googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
 	// GitHub auth
-	githubClientID: '1ec47189b754f4e3363d',
-	githubClientSecret: 'bbe159453c8cdf311567d66c72bfa71c5776727c',
+	githubClientID: process.env.GITHUB_CLIENT_ID,
+	githubClientSecret: process.env.GITHUB_CLIENT_SECRET,
 	// Mango
 	mongoURI: process.env.MANGO_URI,
 	// Cookie
